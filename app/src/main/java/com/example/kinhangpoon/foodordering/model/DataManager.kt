57d988bb127e5b9ad45cda2386ddeec6b97baa21
kinhang <kp1809@nyu.edu>
2018-03-16 14:44:22 -0500

    kinhang update1

--- app/src/main/java/com/example/kinhangpoon/foodordering/model/DataManager.kt
+++ app/src/main/java/com/example/kinhangpoon/foodordering/model/DataManager.kt
@@ -1,6 +1,7 @@
 package com.example.kinhangpoon.foodordering.model
 
 import android.content.Context
+import android.util.Log
 import com.example.kinhangpoon.foodordering.network.RetrofitInstance
 import com.example.kinhangpoon.foodordering.network.UserService
 import retrofit2.Call
@@ -11,17 +12,22 @@ import retrofit2.Response
 /**
  * Created by KinhangPoon on 15/3/2018.
  */
-class DataManager(context: Context?): IDataManager {
+class DataManager(context: Context): IDataManager {
+    internal var context:Context
+    init {
+        this.context = context
+    }
+
     override fun requestRegister(name: String, password: String, userEmail: String, userAddress: String, userPhone: String) {
         val userService = RetrofitInstance.retrofitInstance!!.create(UserService::class.java)
         val call = userService!!.registerUser(name,userEmail,userPhone,password,userAddress)
         call.enqueue(object : Callback<String>{
             override fun onResponse(call: Call<String>?, response: Response<String>?) {
-
+                Log.i("Register",response?.body().toString())
             }
 
             override fun onFailure(call: Call<String>?, t: Throwable?) {
-
+                Log.i("Register",t.toString())
             }
         })
     }
